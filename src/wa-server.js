6bfba49ee41728f702a0194f09b18bc168680233
Paulo <megamapa@gmail.com>
2023-04-21 19:31:22 -0300

    Small changes

--- src/wa-server.js
+++ src/wa-server.js
@@ -10,13 +10,80 @@ async function GetDate() {
 	return new Date(new Date().getTime() - (offset*60*1000)).toISOString().replace(/T/,' ').replace(/\..+/, '');
 }
 
-// Publish update status
-async function PublishUpdate() {
-	san.publish('san:server_update','{"name":"'+process.title+'","version":"'+Version+'","uptime":"'+Math.floor(OS.uptime()/60)+'"}');
+const greetingMessages = () => {
+	let greetings = ['Posso ajudar?', 'Precisa de ajuda?', 'Pode perguntar estou aqui pra ajudar.'];
+	let h = new Date().getHours();
+	return (h <= 5) ? 'Boa madrugada' :
+		   (h < 12) ? 'Bom dia' :
+		   (h < 18) ? 'Boa tarde' :
+		   'Boa noite';
 }
 
+// Publish update status
+async function PublishUpdate() {
+	GetDate().then(dte => {
+		let uptime = Date.parse(dte) - starttime;
+		pub.publish('san:server_update','{"name":"'+process.title+'","version":"'+Version+'","ipport":"WHATSAPP","uptime":"'+Math.floor(uptime/60000)+'"}');
+	});
+}
+
+// Send message to whatsapp
+async function SendMsg(number, message) {
+	let num = number.includes('@c.us') ? number : `${number}@c.us`;
+	whatsapp.sendMessage(num, message);
+}
+
+/****************************************************************************************************/
+/* Read enviroment variables																		*/
+/****************************************************************************************************/
+const dotenv = require('dotenv');
+dotenv.config();
+
+/****************************************************************************************************/
+/* Create and open Redis connection																	*/
+/****************************************************************************************************/
+const Redis = require('ioredis');
+const hub = new Redis({host:process.env.RD_host, port:process.env.RD_port, password:process.env.RD_pass});
+const pub = new Redis({host:process.env.RD_host, port:process.env.RD_port, password:process.env.RD_pass});
+
+// Updates server status as soon as it successfully connects
+hub.on('connect', function () { PublishUpdate(); GetDate().then(dte => {console.log('\033[30m'+dte+': \033[32mHUB connected.\033[0;0m');}); });
+//hub.set('msg:5511930141514','{"context":"null","last":"0"}');
+// Subscribe
+hub.subscribe("msg:device_update","msg:san_message", (err, count) => {
+  if (err) {
+	console.log('\033[30m'+dte+': \033[31mFailed to subscribe: '+ err.message +'\033[0;0m');
+  } 
+});
+
+// Waiting messages
+hub.on("message", (channel, message) => {
+	// Converte para objeto
+	let obj = JSON.parse(message);
+	// Envia a msg
+	switch (channel) {
+		case 'msg:device_update' :
+			SendMsg(obj.num, obj.msg);
+			break;
+
+		case 'msg:san_message' :
+			SendMsg(process.env.WA_chanel, obj.msg);
+			break;
+	  
+  	}
+	// Atualiza contadores
+	msgsout++;
+	bytsout+=obj.msg.length;
+});
+
+/****************************************************************************************************/
+/* Create and open MySQL connection																	*/
+/****************************************************************************************************/
+const mysql = require('mysql');
+const db = mysql.createPool({host:process.env.DB_host, database:process.env.DB_name, user:process.env.DB_user, password:process.env.DB_pass, connectionLimit:10});
+
 // Initialize global variables
-var numdev=0,msgsin=0,msgsout=0,bytsin=0,bytsout=0,bytserr=0;
+var starttime=0,numdev=0,msgsin=0,msgsout=0,bytsin=0,bytsout=0,bytserr=0;
 
 // Update statistics ever 60s
 setInterval(function() {
@@ -27,7 +94,7 @@ setInterval(function() {
 			// Update database
 			db.getConnection(function(err,connection){
 				if (!err) {
-					connection.query('INSERT INTO syslog (datlog,server,version,ipport,devices,msgsin,msgsout,bytsin,bytsout,bytserr) VALUES (?,?,?,?,?,?,?,?,?,?)',[dte, process.title, Version, process.env.SrvIP + ':' + process.env.SrvPort, numdev, msgsin, msgsout, bytsin, bytsout, bytserr],function (err, result) {connection.release(); if (err) err => console.error(err);});
+					connection.query('INSERT INTO syslog (datlog,server,version,ipport,devices,msgsin,msgsout,bytsin,bytsout,bytserr) VALUES (?,?,?,?,?,?,?,?,?,?)',[dte, process.title, Version, 'whatsapp', numdev, msgsin, msgsout, bytsin, bytsout, bytserr],function (err, result) {connection.release(); if (err) err => console.error(err);});
 				}
 				msgsin=0;
 				msgsout=0;
@@ -37,53 +104,83 @@ setInterval(function() {
 			});
 },60000);
 
-// Read enviroment variables
-const dotenv = require('dotenv');
-dotenv.config();
-
-// Create and open Redis connection
-const Redis = require('ioredis');
-const san = new Redis({host:process.env.RD_host, port:process.env.RD_port, showFriendlyErrorStack: true });
-
-// Updates server status as soon as it successfully connects
-san.on('connect', function () { PublishUpdate(); });
-
-// Create and open MySQL connection
-const mysql = require('mysql');
-const db = mysql.createPool({host:process.env.DB_host, database:process.env.DB_name, user:process.env.DB_user, password:process.env.DB_pass, connectionLimit:10});
-
 /****************************************************************************************************/
 /* Open whatsapp instance																			*/
 /****************************************************************************************************/
-const { Client } = require('whatsapp-web.js');
-
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: { headless: false }
+const { Client, LocalAuth, Location, Buttons } = require('whatsapp-web.js');
+const whatsapp = new Client({
+    puppeteer: { headless: false },
+	authStrategy: new LocalAuth()
 });
 
-client.on('qr', (qr) => {
-    // Generate and scan this code with your phone
-    console.log('QR RECEIVED', qr);
+const qrcode = require('qrcode-terminal');
+whatsapp.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+whatsapp.on('ready', () => {
+	GetDate().then(dte => {	console.log('\033[30m'+dte+': \033[32mWhatsapp connected.\033[0;0m');
+							console.log('\033[30m'+dte+': \033[32mWaiting clients...\033[0;0m'); });
 });
 
-client.on('message', msg => {
-    if (msg.body == '!ping') {
-        msg.reply('pong');
-    }
+const _bom_x = 1;
+const _oi = 2;
+
+async function SendMsg(number, message) {
+	let num = number.includes('@c.us') ? number : `${number}@c.us`;
+	whatsapp.sendMessage(num, message);
+}
+
+whatsapp.on('message', async msg => {
+	// Get sender
+	let num = msg.from.split('@')[0];
+	// Verifica se existe
+	hub.exists('msg:'+num, async function (err, result) {
+		if (result) {
+			// Simulates typing in the chat
+			const chat = await msg.getChat();
+			chat.sendStateTyping();
+
+			let res = 0;
+			// Check words
+			let words = msg.body.toLowerCase().split(" ");
+			words.map(w => {
+				switch (w) {
+					case 'oi' :
+					case 'ola' :
+					case 'ai' :
+					case 'conseguiu' :
+						res = _oi;
+						break;
+
+					case 'boa' :	
+					case 'bom' :
+					case 'bon' :
+						res = _bom_x;
+						break;
+	
+				}
+			});
+			// stops typing in the chat
+			chat.clearState();	
+			if (res==_bom_x) {whatsapp.sendMessage(msg.from, greetingMessages());}
+
+		}
+	});
 });
 
-client.initialize();
+whatsapp.initialize();
 
-// Show parameters and waiting clients
+/****************************************************************************************************/
+/* 	Show parameters and waiting clients																*/
+/****************************************************************************************************/
 const OS = require('os');
 GetDate().then(dte => {
-	console.log('\033[1;30m'+dte+': \033[0;31m================================');
-	console.log('\033[1;30m'+dte+': \033[0;31m' + 'APP : ' + process.title + ' ('+Version+')');
-	console.log('\033[1;30m'+dte+': \033[0;31m' + 'CPUs: '+ OS.cpus().length);
-	console.log('\033[1;30m'+dte+': \033[0;31m================================');
-	console.log('\033[1;30m'+dte+': \033[0;31mWaiting clients...\033[0;0m');});
+	// Save start datetime
+	starttime = Date.parse(dte);
+	// Show parameters and waiting clients
+	console.log('\033[30m'+dte+': \033[37m================================');
+	console.log('\033[30m'+dte+': \033[37m' + 'APP : ' + process.title + ' ('+Version+')');
+	console.log('\033[30m'+dte+': \033[37m' + 'IP/Port : WHATSAPP');
+	console.log('\033[30m'+dte+': \033[37m' + 'CPUs: '+ OS.cpus().length);
+	console.log('\033[30m'+dte+': \033[37m================================\033[0;0m');});
