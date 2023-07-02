9e57ab96cb69ca270d813daf36acd84db04f8971
Paulo <megamapa@gmail.com>
2023-07-01 22:46:21 -0300

    Pequenas mudanças

--- src/wa-server.js
+++ src/wa-server.js
@@ -48,7 +48,7 @@ const pub = new Redis({host:process.env.RD_host, port:process.env.RD_port, passw
 
 // Updates server status as soon as it successfully connects
 hub.on('connect', function () { PublishUpdate(); GetDate().then(dte => {console.log('\033[36m'+dte+': \033[32mHUB connected.\033[0;0m');}); });
-//hub.set('msg:5511930141514','{"context":"null","last":"0"}');
+
 // Subscribe
 hub.subscribe("msg:device_update","msg:san_message", (err, count) => {
   if (err) {
@@ -134,39 +134,34 @@ async function SendMsg(number, message) {
 whatsapp.on('message', async msg => {
 	// Get sender
 	let num = msg.from.split('@')[0];
-	// Verifica se existe
-	hub.exists('msg:'+num, async function (err, result) {
-		if (result) {
-			// Simulates typing in the chat
-			const chat = await msg.getChat();
-			chat.sendStateTyping();
+	// Simulates typing in the chat
+	const chat = await msg.getChat();
+	chat.sendStateTyping();
 
-			let res = 0;
-			// Check words
-			let words = msg.body.toLowerCase().split(" ");
-			words.map(w => {
-				switch (w) {
-					case 'oi' :
-					case 'ola' :
-					case 'ai' :
-					case 'conseguiu' :
-						res = _oi;
-						break;
+	let res = 0;
+	// Check words
+	let words = msg.body.toLowerCase().split(" ");
+	words.map(w => {
+		switch (w) {
+			case 'oi' :
+			case 'ola' :
+			case 'ai' :
+			case 'conseguiu' :
+				 res = _oi;
+				 break;
 
-					case 'boa' :	
-					case 'bom' :
-					case 'bon' :
-						res = _bom_x;
-						break;
+			case 'boa' :	
+			case 'bom' :
+			case 'bon' :
+				 res = _bom_x;
+				 break;
 	
-				}
-			});
-			// stops typing in the chat
-			chat.clearState();
-			if (res==_bom_x) {whatsapp.sendMessage(msg.from, greetingMessages());}
-
 		}
-	});
+	});	
+	// stops typing in the chat
+	chat.clearState();
+	
+	if (res==_bom_x) {whatsapp.sendMessage(msg.from, greetingMessages());}
 });
 
 whatsapp.initialize();
@@ -180,7 +175,7 @@ GetDate().then(dte => {
 	starttime = Date.parse(dte);
 	// Show parameters and waiting clients
 	console.log('\033[36m'+dte+': \033[37m================================');
-	console.log('\033[36m'+dte+': \033[37m' + 'APP : ' + process.title + ' ('+Version+')');
-	console.log('\033[36m'+dte+': \033[37m' + 'IP/Port : WHATSAPP');
-	console.log('\033[36m'+dte+': \033[37m' + 'CPUs: '+ OS.cpus().length);
+	console.log('\033[36m'+dte+': \033[37mAPP : ' + process.title + ' ('+Version+')');
+	console.log('\033[36m'+dte+': \033[37mIP/Port : WHATSAPP');
+	console.log('\033[36m'+dte+': \033[37mCPUs: '+ OS.cpus().length);
 	console.log('\033[36m'+dte+': \033[37m================================\033[0;0m');});
